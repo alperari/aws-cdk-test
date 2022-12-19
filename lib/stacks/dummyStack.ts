c58f77957cec7fabb7ce06f5f261e460df21dd41
Alper ARI <alper.hamza.ari@gmail.com>
2022-12-19 15:28:05 +0300

    Add 2nd dummy lambda to test deployment

--- lib/stacks/dummyStack.ts
+++ lib/stacks/dummyStack.ts
@@ -14,5 +14,14 @@ export class DummyLambdaStack extends Stack {
       code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
       handler: "hello.handler", // file is "hello", function is "handler"
     });
+
+    const dummyLambda2 = new lambda.Function(this, "dummy-lambda-2", {
+      // "HelloHandler" is  local identity of the construct,  It’s an ID that has to be unique amongst construct within the same scope
+
+      //fields below are "props" of the current stack
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
   }
 }
